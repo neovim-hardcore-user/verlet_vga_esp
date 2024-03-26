1d082f472d25cb815349bb4d8b2341ed1756518c
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2024-03-26 15:32:12 +0100

    more performance

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # verlet_vga_esp
  pretty performant physics on the esp32 using bitluni's awesome vga library
 
-enable O2 and funroll-loops for better performance (for some reason O3 and Ofast are slower)
+enable Ofast and funroll-loops for better performance
--- verlet_vga_esp.ino
+++ verlet_vga_esp.ino
@@ -1,11 +1,11 @@
 #include <ESP32Lib.h>
 #include <Ressources/Font6x8.h>
 #include <Ressources/CodePage437_8x16.h>
-
+#include <esp_task_wdt.h>
 
 
 struct point {
-  int32_t x, y, px, py;
+  int x, y, px, py;
   unsigned short color;
 };
 
@@ -13,21 +13,21 @@ struct point {
 static constexpr uint8_t FIXED_POINT = 11;
 static constexpr uint16_t MAX_POINTS = 1000;
 
-static constexpr int32_t w = 200 << FIXED_POINT;
-static constexpr int32_t h = 150 << FIXED_POINT;
+static constexpr int w = 200 << FIXED_POINT;
+static constexpr int h = 150 << FIXED_POINT;
 
 static point points[MAX_POINTS];
 
 
-int32_t constexpr r = 2 << FIXED_POINT;
-int32_t constexpr r2 = r * 2;
-int32_t constexpr fr = r >> FIXED_POINT;
-int32_t constexpr fr2 = fr * 2;
+int constexpr r = 2 << FIXED_POINT;
+int constexpr r2 = r * 2;
+int constexpr fr = r >> FIXED_POINT;
+int constexpr fr2 = fr * 2;
 
 static int16_t circle_lines[fr + 1];
 
-static constexpr int32_t GRID_X = (w / (r * 2 + 1)) + 1;
-static constexpr int32_t GRID_Y = (h / (r * 2 + 1)) + 1;
+static constexpr int GRID_X = (w / (r * 2 + 1)) + 1;
+static constexpr int GRID_Y = (h / (r * 2 + 1)) + 1;
 
 
 static constexpr uint16_t GRID_MAX = 3;
@@ -35,9 +35,11 @@ static constexpr uint16_t GRID_MAX = 3;
 static uint16_t grid_dict[GRID_X][GRID_Y][GRID_MAX + 1];
 
 
+static constexpr int boundary_max_x = w - r;
+static constexpr int boundary_max_y = h - r;
 
-static uint32_t frame_time = 0;
-static uint32_t current_time = 0;
+static uint frame_time = 0;
+static uint current_time = 0;
 static float delta_time = 0;
 
 static constexpr bool logger = 1;
@@ -58,55 +60,64 @@ void initialize() {
   for (uint16_t x = 0; x < MAX_POINTS; ++x) {
     auto p = &points[x];
 
-    p->x = (x % (GRID_X - 2) * fr * 2 + fr) << FIXED_POINT;
-    p->y = h - ((x / (GRID_X - 2) * fr * 2 + fr) << FIXED_POINT);
+    p->x = (x % (GRID_X - 1) * fr * 2 + fr) << FIXED_POINT;
+    p->y = h - ((x / (GRID_X - 1) * fr * 2 + fr) << FIXED_POINT);
     p->px = p->x + (rand() % 2) - 1;
     p->py = p->y + (rand() % 2) - 1;
-    p->color = vga.RGB(((float)p->x / (float)w * 255.0f),
-                       ((float)(w - (p->y >> FIXED_POINT)) / (float)(h >> FIXED_POINT) * 255.0f),
-                       255);
+    p->color = (vga.RGB(((float)p->x / (float)w * 255.0f),
+                        ((float)(w - (p->y >> FIXED_POINT)) / (float)(h >> FIXED_POINT) * 255.0f),
+                        255) & vga.RGBMask) | vga.SBits;
     //p->color = vga.RGB(rand() % 255, rand() % 255, rand() % 255);
   }
 }
 
 
 void prerender_circle() {
-  for (uint16_t y = 0; y <= fr; ++y) {
+  for (int y = 0; y <= fr; ++y) {
     circle_lines[y] = (int)sqrt(fr * fr - y * y);
   }
 }
 
 void draw_fast_circles() {
-  int16_t xr = *circle_lines;
-  for (int16_t line = -xr; line <= xr; ++line) {
-    for (uint16_t i = 0; i < MAX_POINTS; ++i) {
+  int16_t xr;
+
+  for (int a = 1; a <= fr; ++a) {
+    xr = circle_lines[a];
+    for (int line = -xr; line <= xr; ++line) {
+      for (uint16_t i = 0; i < MAX_POINTS; ++i) {
+        auto p = &points[i];
+
+        int x = (p->x >> FIXED_POINT) + line;
+        int y = (p->y >> FIXED_POINT);
+
+        unsigned int color = p->color;
+        int new_x = x ^ 1;
+
+        int new_y = y + a;
+        if (new_y < vga.yres && new_y >= 0)
+          vga.backBuffer[new_y][new_x] = color;
+
+        new_y = y - a;
+        if (new_y < vga.yres && new_y >= 0)
+          vga.backBuffer[new_y][new_x] = color;
+      }
+    }
+  }
+  xr = *circle_lines;
+
+  for (int line = -xr; line <= xr; ++line) {
+    for (int i = 0; i < MAX_POINTS; ++i) {
       auto p = &points[i];
       unsigned int x = p->x >> FIXED_POINT;
       unsigned int y = p->y >> FIXED_POINT;
       if (x < vga.xres && y < vga.yres)
-        vga.backBuffer[y][(x + line) ^ 1] = (p->color & vga.RGBMask) | vga.SBits;
-    }
-  }
-  for (uint16_t a = 1; a <= fr; ++a) {
-    xr = circle_lines[a];
-    for (int16_t line = -xr; line <= xr; ++line) {
-      for (uint16_t i = 0; i < MAX_POINTS; ++i) {
-        auto p = &points[i];
-
-        unsigned int x = (p->x >> FIXED_POINT) + line;
-        if (x >= vga.xres) continue;
-        unsigned int y = (p->y >> FIXED_POINT);
-
-        uint16_t c = (p->color & vga.RGBMask) | vga.SBits;
-        if (y + a < vga.yres)
-          vga.backBuffer[y + a][x ^ 1] = c;
-
-        if (y - a < vga.yres)
-          vga.backBuffer[y - a][x ^ 1] = c;
-      }
+        vga.backBuffer[y][(x + line) ^ 1] = p->color;
     }
   }
 }
+
+
+
 
 
 void draw_function(void *param) {
@@ -119,7 +130,7 @@ void draw_function(void *param) {
 
 
     if (logger) {
-      delta_time = (float)(delta_time * 100 + frame_time) / 101;
+      delta_time = (float)(delta_time * 200 + frame_time) / 201;
       vga.setFont(CodePage437_8x16);
 
 
@@ -139,22 +150,19 @@ void draw_function(void *param) {
     vga.show();
 
     draw_finished = 1;
-    vTaskDelay(1);
   }
 }
 
 
-unsigned int isqrt(unsigned int num) {
+int isqrt(int num) {
   float x = (float)num;
   float xhalf = x * 0.5f;
-  int32_t i = *(int32_t *)&x;
+  int i = *(int *)&x;
   i = 0x5f3759df - (i >> 1);
   x = *(float *)&i;
   x = 1.0f / (x * (1.5f - (xhalf * x * x)));
-  return (unsigned int)x;
+  return (int)x;
 }
-
-
 
 
 void update_points() {
@@ -166,11 +174,11 @@ void update_points() {
   int16_t ay = sin(rotation) * gravity;
   //ay = 0;
 
-  for (uint16_t a = 0; a < MAX_POINTS; ++a) {
+  for (int a = 0; a < MAX_POINTS; ++a) {
     auto pa = &points[a];
 
-    int32_t vx = pa->x - pa->px + ax;
-    int32_t vy = pa->y - pa->py + ay;
+    int vx = pa->x - pa->px + ax;
+    int vy = pa->y - pa->py + ay;
 
     pa->px = pa->x;
     pa->py = pa->y;
@@ -179,52 +187,40 @@ void update_points() {
     pa->y += vy;
 
 
-    if (pa->x >= w - r) {
-      pa->x = w - r;
-    }
-    if (pa->x <= r) {
-      pa->x = r;
-    }
+    pa->x = (pa->x < r) ? r : (pa->x > boundary_max_x) ? boundary_max_x : pa->x;
+    pa->y = (pa->y < r) ? r : (pa->y > boundary_max_y) ? boundary_max_y : pa->y;
 
 
-    if (pa->y >= h - r) {
-      pa->y = h - r;
-    }
-    if (pa->y <= r) {
-      pa->y = r;
-    }
+    int grid_x = pa->x / r2;
+    int grid_y = pa->y / r2;
 
 
-    uint16_t grid_x = pa->x / r2;
-    uint16_t grid_y = pa->y / r2;
-
-
-    const int16_t grid_x_start = max(grid_x - 1, 0);
-    const int16_t grid_x_end = min(grid_x + 1, GRID_X - 1);
-    const int16_t grid_y_start = max(grid_y - 1, 0);
-    const int16_t grid_y_end = min(grid_y + 1, GRID_Y - 1);
+    int grid_x_start = max(grid_x - 1, 0);
+    int grid_x_end = min(grid_x + 1, GRID_X - 1);
+    int grid_y_start = max(grid_y - 1, 0);
+    int grid_y_end = min(grid_y + 1, GRID_Y - 1);
 
 
 
-    for (int16_t x = grid_x_start; x <= grid_x_end; ++x) {
-      for (int16_t y = grid_y_start; y <= grid_y_end; ++y) {
-        for (int u = 1; u <= grid_dict[x][y][0]; ++u) {
+    for (int x = grid_x_start; x <= grid_x_end; ++x) {
+      for (int y = grid_y_start; y <= grid_y_end; ++y) {
+        for (int u = 1; u <= *grid_dict[x][y]; ++u) {
           auto pb = &points[grid_dict[x][y][u]];
 
-          int32_t dx = pa->x - pb->x;
-          if (abs(dx) >= r2) continue;
+          int dx = pa->x - pb->x;
+          if (dx >= r2 || dx <= -r2) continue;
 
-          int32_t dy = pa->y - pb->y;
-          if (abs(dy) >= r2) continue;
+          int dy = pa->y - pb->y;
+          if (dy >= r2 || dy <= -r2) continue;
 
-          int32_t dot = dx * dx + dy * dy;
-          if (dot == 0 || dot >= r2 * r2) continue;
+          int dot = dx * dx + dy * dy;
+          if (dot == 0 || dot >= (r2 * r2)) continue;
 
-          int32_t l = isqrt(dot) - 10 * fr;
-          int32_t factor = (((l - r2) << 15) / l) >> 1;
+          int l = isqrt(dot) - (fr * 10);
+          int factor = ((l - r2) << 15) / l;
 
-          dx = (dx * factor) >> 15;
-          dy = (dy * factor) >> 15;
+          dx = (dx * factor) >> 16;
+          dy = (dy * factor) >> 16;
 
           pb->x += dx;
           pb->y += dy;
@@ -235,14 +231,16 @@ void update_points() {
       }
     }
 
-
-    if (grid_dict[grid_x][grid_y][0] == GRID_MAX) continue;
-    ++grid_dict[grid_x][grid_y][0];
-    grid_dict[grid_x][grid_y][grid_dict[grid_x][grid_y][0]] = a;
+    uint16_t *grid_ptr = grid_dict[grid_x][grid_y];
+    if (*grid_ptr == GRID_MAX) continue;
+    ++*grid_ptr;
+    grid_ptr[*grid_ptr] = a;
   }
 }
 
 void setup() {
+  disableCore0WDT();
+  disableCore1WDT();
 
   vga.setFrameBufferCount(2);
   vga.init(vga.MODE200x150, vga.VGABlackEdition);
@@ -257,7 +255,7 @@ void setup() {
 
 
 void loop() {
-  uint32_t time = millis();
+  uint time = millis();
   frame_time = time - current_time;
   current_time = time;
   while (!draw_finished) delay(0);
